613468532af9107a0d5c95f2e38332a13a5791fd
25ooy <1279485126@qq.com>
2026-09-14 16:53:35 +0800

    左侧菜单栏

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { Menu, Promotion } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div class="common-layout">
@@ -18,9 +20,60 @@
 
       <el-container>
         <!-- 左侧菜单 -->
-        <el-aside width="200px" class="aside"> 左侧菜单栏 </el-aside>
+        <el-aside width="200px" class="aside">
+          <el-menu router>
+            <!-- 首页菜单 -->
+            <el-menu-item index="/index">
+              <el-icon><Promotion /></el-icon>首页
+            </el-menu-item>
 
-        <el-main> 右侧核心展示区域 </el-main>
+            <!-- 班级学员管理菜单 -->
+            <el-sub-menu index="/manage">
+              <template #title>
+                <el-icon><Menu /></el-icon>班级学员管理
+              </template>
+              <el-menu-item index="/clazz"
+                ><el-icon><HelpFilled /></el-icon>班级管理</el-menu-item
+              >
+              <el-menu-item index="/stu"
+                ><el-icon><UserFilled /></el-icon>学员管理</el-menu-item
+              >
+            </el-sub-menu>
+
+            <!-- 系统信息管理 -->
+            <el-sub-menu index="/system">
+              <template #title>
+                <el-icon><Tools /></el-icon>系统信息管理
+              </template>
+              <el-menu-item index="/dept"
+                ><el-icon><HelpFilled /></el-icon>部门管理</el-menu-item
+              >
+              <el-menu-item index="/emp"
+                ><el-icon><Avatar /></el-icon>员工管理</el-menu-item
+              >
+            </el-sub-menu>
+
+            <!-- 数据统计管理 -->
+            <el-sub-menu index="/report">
+              <template #title>
+                <el-icon><Histogram /></el-icon>数据统计管理
+              </template>
+              <el-menu-item index="/empReport"
+                ><el-icon><InfoFilled /></el-icon>员工信息统计</el-menu-item
+              >
+              <el-menu-item index="/stuReport"
+                ><el-icon><Share /></el-icon>学员信息统计</el-menu-item
+              >
+              <el-menu-item index="/log"
+                ><el-icon><Document /></el-icon>日志信息统计</el-menu-item
+              >
+            </el-sub-menu>
+          </el-menu>
+        </el-aside>
+
+        <el-main>
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
