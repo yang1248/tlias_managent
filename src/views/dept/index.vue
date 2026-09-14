91db412f977067c48f231ed5a84619a69698829b
25ooy <1279485126@qq.com>
2026-09-14 17:43:28 +0800

    部门管理列表查询

--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,5 +1,52 @@
-<script setup></script>
+<template>
+  <h1>部门管理</h1>
+  <div class="container">
+    <el-button type="primary">+ 新增部门</el-button>
+  </div>
 
-<template>部门管理</template>
+  <div class="container">
+    <el-table :data="deptList" border style="width: 100%">
+      <el-table-column type="index" label="序号" width="100" align="center" />
+      <el-table-column prop="name" label="部门名称" width="260" align="center" />
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center" />
+      <el-table-column label="操作" align="center">
+        <template #default>
+          <el-button type="primary" size="small"
+            ><el-icon><Edit /></el-icon>编辑</el-button
+          >
+          <el-button type="danger" size="small"
+            ><el-icon><Delete /></el-icon>删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
 
-<style scoped></style>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+import axios from 'axios'
+
+//钩子函数
+onMounted(() => {
+  search()
+})
+
+//查询
+const search = async () => {
+  const result = await axios.get('https://apifoxmock.com/m1/3128855-1224313-default/depts')
+  if (result.data.code) {
+    //js中隐式类型转换, 0 -> false, 其他数字 -> true
+    deptList.value = result.data.data
+  }
+}
+
+const deptList = ref([])
+</script>
+
+<style scoped>
+.container {
+  margin: 10px 0px;
+}
+</style>
