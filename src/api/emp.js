e5c7c1e70ab1c1767a403d792b952bd16e225a80
25ooy <1279485126@qq.com>
2026-09-15 22:09:25 +0800

    员工列表查询

---
+++ src/api/emp.js
@@ -0,0 +1,13 @@
+import request from '../utils/request'
+
+//查询全部员工
+export const queryPageApi = (name, gender, begin, end, page, pageSize) =>
+  request.get(`/emps?name=${name}&gender=${gender}&begin=${begin}&end=${end}&page=${page}&pageSize=${pageSize}`)
+
+//新增
+
+//根据ID查询员工
+
+//修改
+
+//删除
--- src/views/emp/index.vue
+++ src/views/emp/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, onMounted } from 'vue'
+import { queryPageApi } from '../../api/emp'
 
 //搜索表单对象
 const searchEmp = ref({
@@ -10,9 +11,25 @@ const searchEmp = ref({
   end: ''
 })
 
+//钩子函数
+onMounted(() => {
+  search()
+})
+
 //查询员工列表
-const search = () => {
-  console.log('查询员工列表', searchEmp.value)
+const search = async () => {
+  const result = await queryPageApi(
+    searchEmp.value.name,
+    searchEmp.value.gender,
+    searchEmp.value.begin,
+    searchEmp.value.end,
+    currentPage.value,
+    pageSize.value
+  )
+  if (result.code) {
+    empList.value = result.data.rows
+    total.value = result.data.total
+  }
 }
 
 //清空
@@ -73,6 +90,27 @@ watch(
 //     console.log('user.name changed from', oldVal, 'to', newVal)
 //   }
 // )
+
+//员工列表数据
+const empList = ref([])
+
+//分页
+const currentPage = ref(1) //页码
+const pageSize = ref(10) //每页展示记录数
+const background = ref(true) //背景色
+const total = ref(0) //总记录数
+
+//处理分页变化
+const handleSizeChange = (val) => {
+  pageSize.value = val
+  search()
+}
+
+//处理当前页变化
+const handleCurrentChange = (val) => {
+  currentPage.value = val
+  search()
+}
 </script>
 
 <template>
@@ -105,10 +143,71 @@ watch(
       </el-form-item>
     </el-form>
   </div>
+
+  <!-- 功能按钮 -->
+  <div class="container">
+    <el-button type="primary" @click="">+ 新增员工</el-button>
+    <el-button type="danger" @click="">- 批量删除</el-button>
+  </div>
+
+  <!-- 表格 -->
+  <div class="container">
+    <el-table :data="empList" border style="width: 100%">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column prop="name" label="姓名" width="120" align="center" />
+      <el-table-column label="性别" width="120" align="center">
+        <template #default="scope">
+          {{ scope.row.gender == 1 ? '男' : '女' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="头像" width="120" align="center">
+        <template #default="scope">
+          <img :src="scope.row.image" height="40px" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="deptName" label="所属部门" width="120" align="center" />
+      <el-table-column prop="job" label="职位" width="120" align="center">
+        <template #default="scope">
+          <span v-if="scope.row.job == 1">班主任</span>
+          <span v-else-if="scope.row.job == 2">讲师</span>
+          <span v-else-if="scope.row.job == 3">学工主管</span>
+          <span v-else-if="scope.row.job == 4">教研主管</span>
+          <span v-else-if="scope.row.job == 5">咨询师</span>
+          <span v-else>其他</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="entryDate" label="入职日期" width="180" align="center" />
+      <el-table-column prop="updateDate" label="最后操作时间" width="200" align="center" />
+      <el-table-column label="操作" align="center">
+        <template #default="scope">
+          <el-button type="primary" size="small" @click=""
+            ><el-icon><Edit /></el-icon>编辑</el-button
+          >
+          <el-button type="danger" size="small" @click=""
+            ><el-icon><Delete /></el-icon>删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+  <!-- 分页 -->
+  <div class="container">
+    <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[5, 10, 20, 30, 40, 50, 75, 100]"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
 </template>
 
 <style scoped>
 .container {
-  margin: 20px 0;
+  margin: 10px 0;
 }
 </style>
